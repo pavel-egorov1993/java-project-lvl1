03a1fe2abe3992cef0d50eb47502270b40076d8e
pavel <pavel_egorov1993@mail.ru>
2019-11-16 20:15:18 +0300

    game logic

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -2,8 +2,35 @@ package games;
 
 public class Slot {
 
+    private int cash = 100;
+    private int rate = 10;
+    private int Firstdrum;
+    private int SecondDrum;
+    private int ThirdDrum;
+    private int size = 7;
+
+    Slot(){
+        for(int i = rate;i <= cash;) {
+            System.out.println("У Вас " + cash + "$," + " ставка - " + rate + "$");
+            Firstdrum = (Firstdrum + (int) Math.round(Math.random() * 100)) % size;
+            SecondDrum = (SecondDrum + (int) Math.round(Math.random() * 100)) % size;
+            ThirdDrum = (ThirdDrum + (int) Math.round(Math.random() * 100)) % size;
+            System.out.println("Крутим барабаны! Розыгриш принес следующие результаты:");
+            System.out.println("первый барабан - " + Firstdrum + " второй - " + SecondDrum + " третий - " + ThirdDrum);
+            if(Firstdrum == SecondDrum && Firstdrum == ThirdDrum) {
+                cash+=1000;
+                System.out.println("Поздравляем!!! Ваш выигрыш 1000$, теперь капитал составляет - " + cash + "$");
+                return;
+            }
+            else {
+                cash-=rate;
+                System.out.println("Проигрыш " + rate + "$, Ваш капитал теперь составляет: " + cash + "$");
+            }
+        }
+    }
+
     public static void main(String... __) {
-        System.out.println("Hello, World!");
+        Slot slot = new Slot();
 
     }
 }
