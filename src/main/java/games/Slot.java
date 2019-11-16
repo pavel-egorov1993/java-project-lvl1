4041e64f2cb5123e46f5a8e799de6d5bc86eb9d2
pavel <pavel_egorov1993@mail.ru>
2019-11-16 20:37:56 +0300

    game update

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -4,7 +4,7 @@ public class Slot {
 
     private int cash = 100;
     private int rate = 10;
-    private int Firstdrum;
+    private int FirstDrum;
     private int SecondDrum;
     private int ThirdDrum;
     private int size = 7;
@@ -12,12 +12,12 @@ public class Slot {
     Slot(){
         for(int i = rate;i <= cash;) {
             System.out.println("У Вас " + cash + "$," + " ставка - " + rate + "$");
-            Firstdrum = (Firstdrum + (int) Math.round(Math.random() * 100)) % size;
+            FirstDrum = (FirstDrum + (int) Math.round(Math.random() * 100)) % size;
             SecondDrum = (SecondDrum + (int) Math.round(Math.random() * 100)) % size;
             ThirdDrum = (ThirdDrum + (int) Math.round(Math.random() * 100)) % size;
             System.out.println("Крутим барабаны! Розыгриш принес следующие результаты:");
-            System.out.println("первый барабан - " + Firstdrum + " второй - " + SecondDrum + " третий - " + ThirdDrum);
-            if(Firstdrum == SecondDrum && Firstdrum == ThirdDrum) {
+            System.out.println("первый барабан - " + FirstDrum + " второй - " + SecondDrum + " третий - " + ThirdDrum);
+            if(FirstDrum == SecondDrum && FirstDrum == ThirdDrum) {
                 cash+=1000;
                 System.out.println("Поздравляем!!! Ваш выигрыш 1000$, теперь капитал составляет - " + cash + "$");
                 return;
